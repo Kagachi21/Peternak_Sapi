878fa08ab1ba90de2a4cdc5f0f6f1562af63929e
Kagachi21 <Kagachi21@users.noreply.github.com>
2021-02-22 20:15:12 +0700

    Lanjutkan Samid Yan

---
+++ app/src/main/java/com/example/peternaksapi/api/API.java
@@ -0,0 +1,13 @@
+package com.example.peternaksapi.api;
+
+public class API {
+    public static final String BASE = "http://angon.flow-byte.com/";
+    public static final String LOGIN = BASE+"api/Auth";
+    public static final String REGISTER = BASE+"api/Registrasi";
+    public static final String UPLOADKTP = BASE+"api/UploadKTP";
+    public static final String DAT_SAP = BASE+"api/Sapi";
+    public static final String REVIEW = BASE+"api/Masukkan";
+    public static final String SEWA = BASE+"api/Sewa";
+    public static final String VERIF_SEWA = BASE+"api/Verif_Sewa";
+    public static final String NOTIF = BASE+"api/Notification";
+}
